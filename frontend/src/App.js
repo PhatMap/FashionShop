ebaa6fa1cf67ec3b51bdf4002c07dd9ca34b8e28
PhatMap <phatmap234@gmail.com>
2024-04-16 01:54:04 +0700

    bg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 
 import Home from "./components/Home";
+// import Home from "./components/test";
 import Shop from "./components/Shop";
 
 import ProductDetails from "./components/product/ProductDetails";
@@ -49,6 +50,7 @@ import axios from "axios";
 // Payment
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import BackGround from "./components/layout/BackGround";
 
 function App() {
   const [stripeApiKey, setStripeApiKey] = useState("");
@@ -79,10 +81,10 @@ function App() {
         <Header />
 
         <div className="container container-fluid">
+          <BackGround/>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<Shop />} />{" "}
-            {/* Thêm đường dẫn cho trang Shop */}
+            <Route path="/shop" element={<Shop />} />
             <Route path="/search/:keyword" element={<Shop />} />
             <Route path="/product/:id" element={<ProductDetails />} />
             <Route path="/category/:category" element={<Category />} />
--- frontend/src/components/test.js
+++ frontend/src/components/test.js
@@ -13,314 +13,52 @@ import { useParams, useNavigate } from "react-router-dom"; // Import useNavigate
 import ProductCarousel from "./layout/Carousel";
 
 const Home = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [price, setPrice] = useState([1, 1000]);
-  const [rating, setRating] = useState(0);
-  const [category, setCategory] = useState("");
-  const [cols, setCols] = useState(4);
-  const navigate = useNavigate();
-  const [fiveStarProducts, setFiveStarProducts] = useState([]);
-
-  const dispatch = useDispatch();
-  
-  const {
-    loading,
-    products,
-    error,
-    productsCount,
-    resPerPage,
-    filteredProductsCount,
-  } = useSelector((state) => state.products);
-
-  const { keyword } = useParams();
-
-  useEffect(() => {
-    dispatch(getProducts(keyword, currentPage, price, rating));
-    if (error) {
-      toast.error(error, {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }
-  }, [dispatch, keyword, currentPage, price, rating, error]);
-
-  const renderProducts = () => {
-    return (
-      <div
-        className="row"
-        style={{ gap: "-100px", justifyContent: "space-around" }}
-      >
-        {products.slice(0, 3).map((product) => (
-          <Product
-            key={product._id}
-            product={product}
-            col={cols}
-            className="product-item"
-            // Giảm margin và tăng độ rộng để sản phẩm hiển thị rộng hơn
-            style={{ margin: "10px", width: "calc(100% / 3 - 10px)" }}
-          />
-        ))}
-      </div>
-    );
-  };
-
-
-  useEffect(() => {
-    if(products.length > 0) {
-      const topRatedProducts = products
-        .filter((p) => p.ratings === 5)
-        .sort((a, b) => b.ratings - a.ratings || b.numOfReviews - a.numOfReviews)
-        .slice(0, 3); // Lấy ra 3 sản phẩm đầu tiên
-    
-      setFiveStarProducts(topRatedProducts);
-    }
-  }, [products]);
-  
-
-const renderProductsStar = () => {
-  return (
-    <div className="row">
-      {fiveStarProducts.map((product) => (
-        <Product
-          key={product._id}
-          product={product}
-          col={4} // Giả sử mỗi hàng hiển thị 3 sản phẩm, bạn có thể điều chỉnh số cột phù hợp
-        />
-      ))}
-    </div>
-  );
-};
-
-
-
-
-
-
-
-
-
-
-
-  
-  const handleShowMorestar = () => {
-    navigate("/shop", { state: { fromFiveStar: true } });
-  };
-  
-  
-  
-
-  const [backgroundImages, setBackgroundImages] = useState([
-    "../images/background_image_1.jpg",
-    "../images/background_image_2.jpg",
-    "../images/background_image_3.jpg",
-    "../images/background_image_4.jpg",
-  ]);
-  const [currentBackgroundIndex, setCurrentBackgroundIndex] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentBackgroundIndex(
-        (prevIndex) => (prevIndex + 1) % backgroundImages.length
-      );
-    }, 5000);
-
-    return () => clearInterval(intervalId);
-  }, [backgroundImages.length]);
-
-  const handleShowMore = () => {
-    navigate("/shop");
-  };
-
-  const defaultProductsGrid = <div className="row">{renderProducts()}</div>;
-
-  const isSearchKeyword = keyword && keyword.trim() !== "";
-
-  const productsGrid = isSearchKeyword ? renderProducts() : defaultProductsGrid;
-
-  let count = productsCount;
-  if (isSearchKeyword) {
-    count = filteredProductsCount;
-  }
-
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [categories] = useState([
-    {
-      name: "Trousers",
-      images: [
-        "https://res.cloudinary.com/dfbo1ecn9/image/upload/v1711873017/products/yrldavvbleswf2fozlex.jpg",
-        "https://res.cloudinary.com/dfbo1ecn9/image/upload/v1711872666/products/q4lmrlteuf2guxvrshkt.jpg",
-      ],
-      path: "/category/Trousers",
-    },
-    {
-      name: "Shirt",
-      images: [
-        "https://res.cloudinary.com/dfbo1ecn9/image/upload/v1711872666/products/q4lmrlteuf2guxvrshkt.jpg",
-        "https://res.cloudinary.com/dfbo1ecn9/image/upload/v1711874764/products/plfymvcula6ujq14ouph.webp",
-      ],
-      path: "/category/Shirt",
-    },
-    {
-      name: "Shoe",
-      images: [
-        "https://res.cloudinary.com/dfbo1ecn9/image/upload/v1711873017/products/yrldavvbleswf2fozlex.jpg",
-        "https://res.cloudinary.com/dfbo1ecn9/image/upload/v1711874764/products/plfymvcula6ujq14ouph.webp",
-      ],
-      path: "/category/Shoe",
-    },
-  ]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % 2); // Giả sử mỗi danh mục có 2 ảnh
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <Fragment>
-      <ToastContainer />
-
-      {loading ? (
-        <Loader />
-      ) : (
-        <Fragment>
-          <MetaData title={"Home"} />
-          <h1
-            id="products_heading"
-            style={{
-              fontSize: "24px",
-              fontWeight: "bold",
-              color: "#333",
-              textAlign: "center",
-              textTransform: "uppercase",
-              margin: "40px 0",
-              textShadow: "1px 1px 2px rgba(0, 0, 0, 0.2)",
-            }}
-          >
-            <div>
-              {/* Hiển thị hình ảnh từ mảng backgroundImages */}
-              <img
-                src={backgroundImages[currentBackgroundIndex]}
-                alt="Background Image"
-                style={{ width: "100%", maxHeight: "500px" }}
-              />
-            </div>
-            <h1
-              id="products_heading"
-              style={{
-                fontSize: "24px",
-                fontWeight: "bold",
-                color: "#333",
-                textAlign: "center",
-                textTransform: "uppercase",
-                margin: "40px 0",
-                textShadow: "1px 1px 2px rgba(0, 0, 0, 0.2)"
-              }}
-            ></h1>
-            Sản Phẩm Mới Nhất
-          </h1>
-          <div className="container mt-5">
-            <div className="row">
-              <div className="col-md-3" style={{ marginRight: "-150px" }}>
-                <div
-                  className="px-2"
-                  style={{ width: "200px", marginLeft: "-140px" }}
-                ></div>
-              </div>
-              <div className="col-md-9">
-                {/* Products */}
-                <section id="products">{productsGrid}</section>
-                <button
-                  onClick={handleShowMore}
-                  style={{ float: "right", marginTop: "10px" }}
-                >
-                  Show More
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="home-line-between"></div>
-
-          <h2 style={{
-                fontSize: "24px",
-                fontWeight: "bold",
-                color: "#333",
-                textAlign: "center",
-                textTransform: "uppercase",
-                margin: "40px 0",
-                textShadow: "1px 1px 2px rgba(0, 0, 0, 0.2)"
-              }}>
-            Danh Mục Sản Phẩm
-          </h2>
-          <div
-            className="categories-container"
-            style={{
-              display: "flex",
-              justifyContent: "space-around",
-              marginBottom: "20px",
-            }}
-          >
-            {categories.map((category, index) => (
-              <div
-                key={index}
-                onClick={() => navigate(category.path)}
-                style={{ cursor: "pointer", flex: 1, padding: "10px" }}
-              >
-                <p className="category">{category.name}</p>
-                <img
-                  src={category.images[currentImageIndex]}
-                  alt={category.name}
-                  style={{
-                    width: "100%",
-                    height: "auto",
-                    marginBottom: "10px",
-                  }}
-                />
-              </div>
-            ))}
-          </div>
-          <h2 style={{
-                fontSize: "24px",
-                fontWeight: "bold",
-                color: "#333",
-                textAlign: "center",
-                textTransform: "uppercase",
-                margin: "40px 0",
-                textShadow: "1px 1px 2px rgba(0, 0, 0, 0.2)"
-              }}>Sản Phẩm Được Đánh Giá Cao</h2>
-          <div className="container mt-5">
-            <div className="row">
-              <div className="col-md-3" style={{ marginRight: "-150px" }}>
-                <div
-                  className="px-2"
-                  style={{ width: "80px", marginLeft: "-180px" }}
-                ></div>
-              </div>
-              <div className="col-md-9">
-  
-              {renderProductsStar()}
-                <button
-                  onClick={handleShowMorestar} 
-                  style={{ float: "right", marginTop: "10px" }}
-                >
-                  Show More
-                </button>
-              </div>
-            </div>
-          </div>
-
-  
-        </Fragment>
-      )}
+      <div className="Test">
+        <div className="homebg">
+          <span style={{ "--i": 11 }}></span>
+          <span style={{ "--i": 12 }}></span>
+          <span style={{ "--i": 24 }}></span>
+          <span style={{ "--i": 10 }}></span>
+          <span style={{ "--i": 14 }}></span>
+          <span style={{ "--i": 23 }}></span>
+          <span style={{ "--i": 18 }}></span>
+          <span style={{ "--i": 16 }}></span>
+          <span style={{ "--i": 19 }}></span>
+          <span style={{ "--i": 20 }}></span>
+          <span style={{ "--i": 22 }}></span>
+          <span style={{ "--i": 25 }}></span>
+          <span style={{ "--i": 18 }}></span>
+          <span style={{ "--i": 21 }}></span>
+          <span style={{ "--i": 15 }}></span>
+          <span style={{ "--i": 13 }}></span>
+          <span style={{ "--i": 26 }}></span>
+          <span style={{ "--i": 17 }}></span>
+          <span style={{ "--i": 13 }}></span>
+          <span style={{ "--i": 28 }}></span>
+          <span style={{ "--i": 11 }}></span>
+          <span style={{ "--i": 12 }}></span>
+          <span style={{ "--i": 24 }}></span>
+          <span style={{ "--i": 10 }}></span>
+          <span style={{ "--i": 14 }}></span>
+          <span style={{ "--i": 23 }}></span>
+          <span style={{ "--i": 18 }}></span>
+          <span style={{ "--i": 16 }}></span>
+          <span style={{ "--i": 19 }}></span>
+          <span style={{ "--i": 20 }}></span>
+          <span style={{ "--i": 22 }}></span>
+          <span style={{ "--i": 25 }}></span>
+          <span style={{ "--i": 18 }}></span>
+          <span style={{ "--i": 21 }}></span>
+          <span style={{ "--i": 15 }}></span>
+          <span style={{ "--i": 13 }}></span>
+          <span style={{ "--i": 26 }}></span>
+          <span style={{ "--i": 17 }}></span>
+          <span style={{ "--i": 13 }}></span>
+          <span style={{ "--i": 28 }}></span>
+        </div>
+      </div>
     </Fragment>
   );
 };
