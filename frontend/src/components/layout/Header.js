90327d3aca3e078dd6cc9f6efc64b20b1a19cacb
PhatMap <phatmap234@gmail.com>
2024-04-24 00:05:21 +0700

    home frontend

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useRef, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FaShoppingCart, FaBars } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
@@ -14,6 +14,7 @@ import { getUserCart } from "../../actions/cartActions";
 import LogoutButton from "../user/GoogleLogout";
 
 const Header = () => {
+  const location = useLocation();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -82,172 +83,206 @@ const Header = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  useEffect(() => {
+    const header = document.querySelector("h1");
+
+    if (header) {
+      header.addEventListener("mouseover", (event) => {
+        let iterations = 0;
+        const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        const interval = setInterval(() => {
+          event.target.innerText = event.target.innerText
+            .split("")
+            .map((letter, index) => {
+              if (index < iterations) {
+                return event.target.dataset.value[index];
+              }
+              return letters[Math.floor(Math.random() * 26)];
+            })
+            .join("");
+
+          if (iterations >= event.target.dataset.value.length) {
+            clearInterval(interval);
+          }
+
+          iterations += 1 / 3;
+        }, 30);
+
+        return () => {
+          header.removeEventListener("mouseover", () => {});
+          clearInterval(interval);
+        };
+      });
+    }
+  }, []);
+
   return (
     <Fragment>
       <ToastContainer />
-      <nav
-        className="navbar row"
-        style={isSticky ? { position: "sticky", top: "0", zIndex: "1000" } : {}}
-      >
-        <div className="col-12 col-md-3 ">
-          <div className="navbar-brand logo-container">
-            <Link to="/">
-              <img src="/images/vita.png" alt="No logo" />
+      <nav className={isSticky ? "Header" : ""}>
+        <div className="Header-container">
+          <div className="Header-container-right">
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <h1 data-value="VITASHOP">VITASHOP</h1>
             </Link>
           </div>
+          <div className="Header-container-center">
+            <Search />
+          </div>
+          <div className="Header-container-left">
+            <div className="Header-category">
+              <i
+                className="fa fa-bars burger-menu"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onMouseEnter={handleClick}
+              ></i>
+              <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+              >
+                {categories.map((cate, index) => (
+                  <MenuItem key={index} className="Header-menu-item">
+                    <Link
+                      style={{ textDecoration: "none", color: "black" }}
+                      to={`/category/${cate}`}
+                      onClick={() => {
+                        setAnchorEl(false);
+                      }}
+                    >
+                      {cate}
+                    </Link>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </div>
+            <Link
+              to="/shop"
+              className={
+                location.pathname === "/shop"
+                  ? "Header-shop-link active"
+                  : "Header-shop-link"
+              }
+            >
+              Shop
+            </Link>
 
-          <button
-            id="basic-button"
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-            className="menu-button"
-          >
-            <FaBars style={{ fontSize: "30px", color: "white" }} />
-          </button>
-          <Menu
-            className="menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            {categories.map((cate, index) => (
-              <MenuItem key={index}>
-                <Link
-                  className="dropdown-item text-danger"
-                  to={`/category/${cate}`}
-                  onClick={() => {
-                    setAnchorEl(false);
-                  }}
-                >
-                  {cate}
-                </Link>
-              </MenuItem>
-            ))}
-          </Menu>
-        </div>
-        <Search />
+            <div className="Header-cart-count">
+              <Link to="/cart" className="Header-cart">
+                <FaShoppingCart
+                  className="Header-cart-icon"
+                  onMouseEnter={handleCartOpen}
+                  onMouseLeave={handleCartClose}
+                />
+              </Link>
+              <span className="Header-count">
+                {user ? cartItems.length : 0}
+              </span>
+            </div>
 
-        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-          {/* Nút Shop */}
-          <Link
-            to="/shop"
-            style={{ color: "white" }}
-            className="btn shop_button"
-          >
-            Shop
-          </Link>
-
-          <Link to="/cart">
-            <FaShoppingCart
-              className="ml-3"
-              style={{ fontSize: "30px", color: "white" }}
-              onMouseEnter={handleCartOpen}
-              onMouseLeave={handleCartClose}
-            />
-          </Link>
-          <span
-            className="ml-1"
-            id="cart_count"
-            style={{
-              fontSize: "13px",
-              borderRadius: "50%",
-              background: "white",
-              color: "black",
-            }}
-          >
-            {user ? cartItems.length : 0}
-          </span>
-
-          <Popper className="cart" anchorEl={anchorE2} open={openCart}>
-            {cartItems.map((item, index) => (
-              <MenuItem key={index}>
-                <div className="cart-MenuItem">
-                  <img src={item.image} height="40" width="40" />
-                  <div className="cart-MenuItem-container">
-                    <p>Tên sản phẩm: {item.name.substring(0, 20)}...</p>
-                    <p>Số lượng mua: {item.quantity}</p>
-                    <div className="cart-summary-color">
-                      <p>Màu:</p>
-                      <p>{item.colorName}</p>
-                      <div
-                        style={{
-                          backgroundColor: item.colorHex ?? "transparent",
-                          width: "10px",
-                          height: "10px",
-                          borderRadius: "50%",
-                        }}
-                      ></div>
+            <Popper
+              anchorEl={anchorE2}
+              open={openCart}
+              className="Header-cart-items"
+            >
+              {cartItems.map((item, index) => (
+                <MenuItem key={index}>
+                  <div className="cart-MenuItem">
+                    <img src={item.image} height="40" width="40" />
+                    <div className="cart-MenuItem-container">
+                      <p>Tên sản phẩm: {item.name.substring(0, 20)}...</p>
+                      <p>Số lượng mua: {item.quantity}</p>
+                      <div className="cart-summary-color">
+                        <p>Màu:</p>
+                        <p>{item.color.colorName}</p>
+                        <div
+                          style={{
+                            backgroundColor:
+                              item.color.colorHex ?? "transparent",
+                            width: "10px",
+                            height: "10px",
+                            borderRadius: "50%",
+                          }}
+                        ></div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </MenuItem>
-            ))}
-          </Popper>
+                </MenuItem>
+              ))}
+            </Popper>
 
-          {user ? (
-            <div className="ml-4 dropdown d-inline">
-              <Link
-                to="#!"
-                className="btn dropdown-toggle text-white mr-4"
-                type="button"
-                id="dropDownMenuButton"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                <figure
-                  className="avatar avatar-nav"
-                  style={{ background: "white" }}
+            {user ? (
+              <div className="ml-4 dropdown d-inline">
+                <Link
+                  to="#!"
+                  className="btn dropdown-toggle text-white mr-4"
+                  type="button"
+                  id="dropDownMenuButton"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
                 >
-                  <img
-                    src={user.avatar && user.avatar.url}
-                    alt={user && user.name}
-                    className="rounded-circle"
-                  />
-                </figure>
-                <span>{user && user.name}</span>
-              </Link>
-
-              <div
-                className="dropdown-menu"
-                aria-labelledby="dropDownMenuButton"
-              >
-                {user && user.role === "admin" && (
-                  <Link className="dropdown-item" to="/dashboard">
-                    Dashboard
-                  </Link>
-                )}
-                <Link className="dropdown-item" to="/orders/me">
-                  Orders
-                </Link>
-                <Link className="dropdown-item" to="/me">
-                  Profile
-                </Link>
-                {isGoogleLoggedIn ? (
-                  <LogoutButton />
-                ) : (
-                  <Link
-                    className="dropdown-item text-danger"
-                    to="/"
-                    onClick={logoutHandler}
+                  <figure
+                    className="avatar avatar-nav"
+                    style={{ background: "white" }}
                   >
-                    Logout
+                    <img
+                      src={user.avatar && user.avatar.url}
+                      alt={user && user.name}
+                      className="rounded-circle"
+                    />
+                  </figure>
+                  <span>{user && user.name}</span>
+                </Link>
+
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="dropDownMenuButton"
+                >
+                  {user && user.role === "admin" && (
+                    <Link className="dropdown-item" to="/dashboard">
+                      Dashboard
+                    </Link>
+                  )}
+                  <Link className="dropdown-item" to="/orders/me">
+                    Orders
                   </Link>
-                )}
+                  <Link className="dropdown-item" to="/me">
+                    Profile
+                  </Link>
+                  {isGoogleLoggedIn ? (
+                    <LogoutButton />
+                  ) : (
+                    <Link
+                      className="dropdown-item text-danger"
+                      to="/"
+                      onClick={logoutHandler}
+                    >
+                      Logout
+                    </Link>
+                  )}
+                </div>
               </div>
-            </div>
-          ) : (
-            !loading && (
-              <Link to="/login" className="btn ml-4" id="login_btn">
-                Login
-              </Link>
-            )
-          )}
+            ) : (
+              !loading && (
+                <Link to="/login" className="Header-login">
+                  Login
+                </Link>
+              )
+            )}
+          </div>
         </div>
       </nav>
     </Fragment>
--- frontend/src/components/layout/Search.js
+++ frontend/src/components/layout/Search.js
@@ -1,15 +1,9 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Search = () => {
   const history = useNavigate();
-  const [showInput, setShowInput] = useState(false);
   const [keyword, setKeyword] = useState("");
-  const inputRef = useRef(null);
-
-  const toggleInput = () => {
-    setShowInput(!showInput);
-  };
 
   const searchHandler = (e) => {
     e.preventDefault();
@@ -21,76 +15,21 @@ const Search = () => {
     }
   };
 
-  const handleClickOutside = (e) => {
-    if (inputRef.current && !inputRef.current.contains(e.target)) {
-      setShowInput(false);
-    }
-  };
-
-  useEffect(() => {
-    if (showInput) {
-      inputRef.current.focus();
-      document.addEventListener("mousedown", handleClickOutside);
-    } else {
-      document.removeEventListener("mousedown", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [showInput]);
-
   return (
-    <form onSubmit={searchHandler}>
-      <div style={{ display: "flex" }}>
-        <div className="find-input">
-          {showInput && (
-            <input
-              ref={inputRef}
-              type="text"
-              placeholder="Enter Product Name ..."
-              style={{
-                borderRadius: "50px",
-                backgroundColor: "transparent",
-                color: "white",
-                width: "400px",
-                placeholderTextColor: "white",
-              }}
-              value={keyword}
-              onChange={(e) => setKeyword(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  searchHandler(e); 
-                }
-              }}
-            />
-          )}
-          {keyword && showInput && (
-            <div>
-              <button className="remove-btn" onClick={() => setKeyword("")}>
-                <i className="fa fa-remove" aria-hidden="true"></i>
-              </button>
-            </div>
-          )}
-          {!showInput && (
-            <div>
-              <button
-                id="search_btn"
-                className="btn-find"
-                style={{
-                  borderRadius: "50%",
-                  backgroundColor: "black",
-                  borderWidth: "5px",
-                  borderColor: "white",
-                }}
-                onClick={toggleInput}
-              >
-                <i className="fa fa-search" aria-hidden="true"></i>
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
+    <form className="Search-form" onSubmit={searchHandler}>
+      <input
+        className="Search-input"
+        type="search"
+        placeholder="Search here..."
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            searchHandler(e);
+          }
+        }}
+      />
+      <i className="fa fa-search Search-icon"></i>
     </form>
   );
 };
